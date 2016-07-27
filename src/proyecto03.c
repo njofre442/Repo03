c0492d4b0cb4b3bd89418f2761f3fdfdc342b542
njofre <njofre@njofre-HP-425>
2016-07-27 15:04:16 -0300

    c1

---
+++ src/proyecto03.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : proyecto03.c
+ Author      : Nicolas
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World nico!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
